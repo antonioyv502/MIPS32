d265e84e5480ceb472474149a45cc6f530a99713
antoniov$502 <bryanvasquez797@gmail.com>
2026-04-23 10:48:19 -0700

    Refactor inst_rom module with memory initialization
    Updated the instruction ROM module to include a memory initialization from a specific file and adjusted the address indexing.

--- Sub_Modules/inst_rom.v
+++ Sub_Modules/inst_rom.v
@@ -2,21 +2,21 @@
 
 
 module inst_rom(ce, addr, inst);
-    input  ce;
-    input  [`INST_ADDRBUS] addr;
-    output [`INST_DATABUS] inst;
+    input  ce;                     // chip enable
+    input  [`INST_ADDRBUS] addr;   // address 32 bits
+    output [`INST_DATABUS] inst;   // data 32 bits
     
     reg [`INST_DATABUS] inst;
     
-    reg [`INST_DATABUS] instmem [0:65535];
+    reg [`INST_DATABUS] instmem [0:10];
     
-    //initial $readmemh("inst_rom.data", instmem); //need to make inst_rom.data file
+    initial $readmemh("C:/BAT/CSULA/EE_4480/16_MIPS32_V1/Source/inst_rom.data", instmem); //need to make inst_rom.data file
     
     always @(*) begin 
         if (ce === `CHIP_DISABLE) begin 
             inst = 32'd0;
         end else begin
-            inst = instmem[ addr[15:0] ];
+            inst = instmem[ addr[15:2] ];
         end 
     end    
 endmodule
